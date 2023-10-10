36880318f71d93970a409fc212da17015e128894
alvin <aocasiojr@my.nl.edu>
2023-10-10 13:59:16 -0500

    Test

---
+++ main.py
@@ -0,0 +1,14 @@
+split ="this string has been \nsplit over\nseveral\nlines"
+#fixed the quotation mark
+print(splitString)
+tabbedstring="1\t2\t3\t4\t5"
+#fixed the quotation mark
+print(tabbedstring)
+print('the pet shop owner said"No,no, \'e\'s uh,...he\'s resting".')
+print("the pet shop owner said \"No, no, 'e's uh,...he's resting\".")
+print("""the pet shop owner said "No, no, 'e's uh,...he's resting\".""")
+anotherString = """ this string has been \
+split over \
+several \
+lines"""" #fixed the quotation mark
+print (another String)
